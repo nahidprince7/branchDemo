e56da4fa15c71b47e271e2ee0ff968bfbc5abd04
nahid <nahid.hasan@bracnet.net>
2021-05-18 00:12:28 +0600

    initial commit

---
+++ action.php
@@ -0,0 +1,9 @@
+<?php
+
+if($_POST['name']=="1"){;
+    echo "Eat food";
+    echo "Join Gym";
+}
+else{
+    echo "Sleep only";
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form action="action.php" method="post">
+  <label for="name">First name:</label><br>
+  <input type="text" name="name">
+  <input type="submit" value="Submit">
+</form>
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action.php".</p>
+
+</body>
+</html>
